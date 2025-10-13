6a44525468f35893c433316a73a3337413f8864f
Cossra <cossrca@Gmail.com>
2025-10-12 21:51:28 -0400

    Initial commit – set up project structure and first class

---
+++ src/com/robert/pokedex/model/Pokemon.java
@@ -0,0 +1,29 @@
+package com.robert.pokedex.model;
+
+import java.util.List;
+
+public class Pokemon {
+    private String name;
+    private int level;
+    private String type;
+
+    private List<String> moves;
+    private int hp;
+    private int maxHp;
+    private boolean isFainted;
+
+
+    public Pokemon(String name, int level, String type, List<String> moves, int MaxHp) {
+        this.name = name;
+        this.level = level;
+        this.type = type;
+        this.moves = moves;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.isFainted = false;
+    }
+
+    public void attack(String move){
+        System.out.println(name + " used " + move + "!");
+    }
+}
